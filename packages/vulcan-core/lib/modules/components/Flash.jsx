032a41cc5803b5da77848a8cb7c33ff2a5484b4f
Daniel « Klokie » Grossfeld <github@klokie.com>
2019-03-20 12:27:01 +0100

    fix Flash component for backwards compatibility

--- packages/vulcan-core/lib/modules/components/Flash.jsx
+++ packages/vulcan-core/lib/modules/components/Flash.jsx
@@ -1,52 +1,80 @@
-import { Components, registerComponent } from 'meteor/vulcan:lib';
+import {Components, registerComponent} from 'meteor/vulcan:lib';
 import withMessages from '../containers/withMessages.js';
-import React, { PureComponent } from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
-import { intlShape } from 'meteor/vulcan:i18n';
+import {intlShape} from 'meteor/vulcan:i18n';
 
 class Flash extends PureComponent {
 
   constructor() {
     super();
-    this.dismissFlash = this.dismissFlash.bind(this);
+    this.dismissFlash = this
+      .dismissFlash
+      .bind(this);
   }
 
   componentDidMount() {
-    this.props.markAsSeen && this.props.markAsSeen(this.props.message._id);
+    this.props.markAsSeen && this
+      .props
+      .markAsSeen(this.props.message._id);
   }
 
   dismissFlash(e) {
     e.preventDefault();
-    this.props.clear(this.props.message._id);
+    this
+      .props
+      .clear(this.props.message._id);
   }
 
-  getProperties = () => {
-    const errorObject = this.props.message.content;
-    if (typeof errorObject === 'string') {
-      // if error is a string, use it as message
-      return {
-        message: errorObject,
-        type: 'error'
-      }
-    } else {
-      // else return full error object after internationalizing message
-      const { id, message, properties } = errorObject;
-      const translatedMessage = this.context.intl.formatMessage({ id, defaultMessage: message }, properties);
-      return {
-        ...errorObject,
-        message: translatedMessage,
-      };
+  getProperties = (errorObject = this.props.message.content) => {
+    switch (typeof errorObject) {
+        case 'undefined':
+        console.log("FLASH", this.props.message);
+        // try backwards compatibility
+        if (this.props.message && this.props.message.properties && this.props.message.properties.errorMessage) {
+          return this.getProperties(this.props.message.properties.errorMessage);
+        }
+        return { message: "Unknown error", type: 'error' }
+
+        case 'string' :
+        // if error is a string, use it as message
+        return {message: errorObject, type: 'error'}
+
+        default:
+        // else return full error object after internationalizing message
+        const {id, message, properties} = errorObject;
+        const translatedMessage = this
+          .context
+          .intl
+          .formatMessage({
+            id,
+            defaultMessage: message
+          }, properties);
+        return {
+          ...errorObject,
+          message: translatedMessage
+        };
     }
   }
 
   render() {
 
-    const { message, type = 'danger' } = this.getProperties();
-    const flashType = type === 'error' ? 'danger' : type; // if flashType is "error", use "danger" instead
+    const {
+      message,
+      type = 'danger'
+    } = this.getProperties();
+    const flashType = type === 'error'
+      ? 'danger'
+      : type; // if flashType is "error", use "danger" instead
 
     return (
-      <Components.Alert className="flash-message" variant={flashType} onDismiss={this.dismissFlash}>
-        <span dangerouslySetInnerHTML={{ __html: message }} />
+      <Components.Alert
+        className="flash-message"
+        variant={flashType}
+        onDismiss={this.dismissFlash}>
+        <span dangerouslySetInnerHTML={{
+          __html: message
+        }}/>
       </Components.Alert>
     )
   }
@@ -67,7 +95,11 @@ const FlashMessages = ({messages, clear, markAsSeen}) => {
     <div className="flash-messages">
       {messages
         .filter(message => message.show)
-        .map(message => <Components.Flash key={message._id} message={message} clear={clear} markAsSeen={markAsSeen} />)}
+        .map(message => <Components.Flash
+          key={message._id}
+          message={message}
+          clear={clear}
+          markAsSeen={markAsSeen}/>)}
     </div>
   );
 }
